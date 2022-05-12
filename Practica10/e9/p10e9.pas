1327fe8ef0549fb54078c3f9c76d08bad227a6dd
OakNimroW <74124711+OakNimroW@users.noreply.github.com>
2022-05-12 13:00:52 -0300

    Update p10e9.pas

--- Practica10/e9/p10e9.pas
+++ Practica10/e9/p10e9.pas
@@ -1,11 +1,12 @@
-program p10e1;
+program p10e9;
 begin
   
 end.
 
 {
-  Escribir un programa que lea una secuencia de números enteros terminada en 999 y los almacene en una lista simple. Utilizando la lista creada implementar:
-  a) un módulo que reciba la lista y devuelva como resultado la cantidad de números con 3 dígitos.
-  b) un módulo que reciba la lista y un número y determine si dicho número está o no en la lista. La búsqueda debe terminar al encontrar la primera ocurrencia del número buscado.
-  c) Un módulo que reciba la lista y libere la memoria reservada.
+  Se cuenta con una lista que contiene información de las ventas realizadas por una empresa de venta de pasajes aéreos.   Cada venta está compuesta por un nombre de persona, código de vuelo, categoría de pasaje y número de asiento. La lista puede contener 0, 1 o más registros por cada código de vuelo, y está ordenada por este campo.
+  El costo de un pasaje depende del vuelo y de su categoría. Se dispone de una estructura eficiente que por cada combinación de vuelo (son 30) y categoría (4 por vuelo) se almacena su precio.
+  a) Generar una lista de registros que contenga por cada código de vuelo, el total de pasajes vendidos y el monto total recaudado.
+  b) Calcular e informar los códigos de los 5 vuelos más vendidos.
+  c) Generar una lista de los códigos de vuelos cuya cantidad de pasajes vendidos sea mayor que 46. La lista debe ir generándose ordenada por monto total a medida que se realiza el proceso a).
 }
