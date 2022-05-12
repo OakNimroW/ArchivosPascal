727e897943f3b86a3a16cc46d7ac4226b225cdf2
OakNimroW <74124711+OakNimroW@users.noreply.github.com>
2022-05-12 13:00:48 -0300

    Update p10e6.pas

--- Practica10/e6/p10e6.pas
+++ Practica10/e6/p10e6.pas
@@ -1,11 +1,11 @@
-program p10e1;
+program p10e6;
 begin
   
 end.
 
 {
-  Escribir un programa que lea una secuencia de números enteros terminada en 999 y los almacene en una lista simple. Utilizando la lista creada implementar:
-  a) un módulo que reciba la lista y devuelva como resultado la cantidad de números con 3 dígitos.
-  b) un módulo que reciba la lista y un número y determine si dicho número está o no en la lista. La búsqueda debe terminar al encontrar la primera ocurrencia del número buscado.
-  c) Un módulo que reciba la lista y libere la memoria reservada.
+  Un cine dispone de una lista con la información de los tickets del día (Nro. Caja, nro ticket, monto) correspondientes a sus 6 cajas. Escribir un programa que procese dicha lista y permita:
+  a) Genere una nueva lista con los tickets con montos menores a $1000.
+  b) Calcular e informar el monto promedio recaudado por caja.
+  c) Calcular que cajas que recaudaron más de $12000 con menos de 100 tickets. Luego vuelva a recorrer la lista para generar una nueva con los tickets pertenecientes a las cajas que cumplan dicha condición. Recuerde usar un conjunto para mejorar la eficiencia.
 }
