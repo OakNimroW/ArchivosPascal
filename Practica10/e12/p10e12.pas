f8b66f9f3038734d25dd8b18f360f32b84457f7e
OakNimroW <74124711+OakNimroW@users.noreply.github.com>
2022-05-23 10:18:28 -0300

    Update p10e12.pas

--- Practica10/e12/p10e12.pas
+++ Practica10/e12/p10e12.pas
@@ -1,4 +1,75 @@
 program p10e12;
+
+type
+  lista = ^ nodo;
+  nodo = record
+    dato : integer;
+    sig : lista;
+  end;
+
+
+procedure agregarOrdenado (var l : lista; d : integer);
+  var
+    act,ant,nue : lista;
+  begin
+    new(nue);
+    nue^.dato := d;
+    nue^.sig := nil;
+    act := l;
+    ant := l;
+    while(act <> nil) and (act^.dato > d) do begin
+      ant := act;
+      act := act^.sig;
+    end;
+    nue^.sig := act;
+    if (l = nil) then l := nue
+    else ant^.sig := nue; 
+    
+  end;
+
+procedure unirListas (A,B : lista; var C: lista);
+  var
+    actA, actB : lista;
+  begin
+    actA := A;
+    actB := B;
+    while (actA <> nil) do begin
+      agregarOrdenado(C,actA^.dato);
+      actA := actA^.sig;
+    end;
+    while (actB <> nil) do begin
+      agregarOrdenado(C,actB^.dato);
+      actB := actB^.sig;
+    end;
+  end;
+
+procedure expandirLista (var A : lista ; B : lista);
+  var
+    actB : lista;
+  begin
+    actB := B;
+    while (actB <> nil) do begin
+      agregarOrdenado(A,actB^.dato);
+      actB := actB^.sig;
+    end;
+  end;
+
+procedure purgarNumero (var A : lista; n : integer);
+  var
+    act, ant : lista;
+  begin
+    act := A;
+    ant := nil;
+    while ( act <> nil ) and ( act^.dato < n) do begin
+      ant := act;
+      act := act^.sig;
+    end;
+    while ( act <> nil ) and ( act^.dato = n) do begin
+      ant := act;
+      act := act^.sig;
+      dispose(ant);
+    end;
+  end;
 begin
   
 end.
